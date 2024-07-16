7ce0a1d859069e938b4a2b8ba65d6e831c7f456c
MattFlame <timurmelnichenko@gmail.com>
2024-07-17 00:22:11 +0300

    added first files

---
+++ main.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    return a / b
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+    print("Add: ", add(1, 2))
+    print("Subtract: ", subtract(4, 2))
+    print("Multiply: ", multiply(3, 3))
+    print("Divide: ", divide(10, 2))
